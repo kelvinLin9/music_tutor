854cd2585216387c4650af957ac6103f9a31ff8d
kelvin <kelvin80121@hotmail.com>
2023-07-23 11:46:00 +0800

    新增timeline

--- src/views/BeATeacherStep4.vue
+++ src/views/BeATeacherStep4.vue
@@ -1,5 +1,11 @@
 <template>
-  <h1>恭喜開課完成</h1>
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-12">
+        <h1>恭喜完成開課</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
