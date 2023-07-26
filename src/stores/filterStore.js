42036c3beaefd3f5a3f208e3c651b66d0ea5db03
kelvin <kelvin80121@hotmail.com>
2023-07-26 09:49:51 +0800

    解決取消搜尋上課方式後上課地點還有值的問題

--- src/stores/filterStore.js
+++ src/stores/filterStore.js
@@ -9,16 +9,30 @@ const Pagination = PaginationStore()
 export default defineStore('filterStore', {
   state: () => ({
     selectCityName: '',
-    selectCourseCategory:'',
-    selectCourseName:''
+    selectCourseCategory: '',
+    selectCourseName: '',
+    courseMethod: ['在學生家', '在老師家', '線上'],
+    selectCourseMethod: ''
   }),
   actions: {
+    selectCityNameCancel () {
+      // 解決取消搜尋上課方式後上課地點還有值的問題
+      if (this.selectCourseMethod === '') {
+        this.selectCityName = ''
+      }
+    }
   },
   getters: {
     filterData () {
-      return data.coursesData.filter((item) => {
-        return item.cityName.match(this.selectCityName) && item.courseCategory.match(this.selectCourseCategory) && item.courseName.match(this.selectCourseName)
-      })
-    }
+      if (this.selectCourseMethod === '') {
+        return data.coursesData.filter((item) => {
+          return item.cityName.match(this.selectCityName) && item.courseCategory.match(this.selectCourseCategory) && item.courseName.match(this.selectCourseName) 
+        }) 
+      } else {
+        return data.coursesData.filter((item) => {
+          return item.cityName.match(this.selectCityName) && item.courseCategory.match(this.selectCourseCategory) && item.courseName.match(this.selectCourseName) && item.courseMethod.includes(this.selectCourseMethod)
+        })
+      }
+    },
   }
 })
