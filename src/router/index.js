1ca7f299f05f6a19ae27aaa4dbf640446cf8dee7
kelvin <kelvin80121@hotmail.com>
2023-07-28 01:25:34 +0800

    開課程序更新

--- src/router/index.js
+++ src/router/index.js
@@ -42,6 +42,11 @@ const router = createRouter({
           ]
         },
         {
+          path: 'AllCoursesDemo',
+          name: 'AllCoursesDemo',
+          component: () => import('../views/AllCoursesDemo.vue')
+        },
+        {
           path: 'AllCourses',
           name: 'AllCourses',
           component: () => import('../views/AllCourses.vue')
