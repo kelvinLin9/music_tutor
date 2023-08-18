27f9bfafb4bbee9c6cc0d17772ba1281612fdcd9
kelvin <kelvin80121@hotmail.com>
2023-08-18 10:58:55 +0800

    新增行事曆功能

---
+++ src/components/SetUpClassScheduleModal2.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="modal fade" id="SetUpClassSchedule2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">設定上課時間</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+
+
+
+
+
+        <div class="block">
+      <span class="demonstration">Use value-format</span>
+      <div class="demonstration">Value：{{ classScheduleTime }}</div>
+      <el-date-picker
+        v-model="classScheduleTime"
+        type="datetime"
+        placeholder="Pick a Date"
+        format="YYYY/MM/DD hh:mm"
+        value-format="YYYY-MM-DD hh:mm"
+      />
+    </div>
+
+        
+
+
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" 
+                data-bs-target="#SetUpClassSchedule" 
+                data-bs-toggle="modal">
+          取消
+        </button>
+        <button class="btn btn-primary" 
+                data-bs-dismiss="modal"
+                @click="UpDateClassSchedule()">
+          確定
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+    
+<script>
+import { mapState, mapActions, mapWritableState } from 
+'pinia' 
+import dataStore from '../stores/dataStore';
+
+
+export default {
+  data () {
+    return {
+      value:''
+    }
+  },
+  computed: {
+    ...mapWritableState(dataStore, ['classScheduleTime'])
+  },
+  methods: {  
+    ...mapActions(dataStore, ['UpDateClassSchedule']),
+    test() {
+      this.value = (time, Date) => {
+        return time.getTime() > Date.now()
+      }
+    }
+  },
+  created() {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
