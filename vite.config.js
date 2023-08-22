66f433926df4aadfa39cf9b1e06c281fc6173a66
kelvin <kelvin80121@hotmail.com>
2023-08-22 16:37:25 +0800

    repo改名

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: '/music_tutor_test/',
+  base: '/music_tutor/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
