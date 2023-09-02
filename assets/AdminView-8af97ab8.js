81f215997ebf9a53dac0a7840d09d2a82ae61402
kelvin <kelvin80121@hotmail.com>
2023-09-02 13:04:26 +0800

    deploy

--- assets/AdminView-8af97ab8.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as _,c as n,e as c}from"./index-df6e219a.js";const s={},r={class:"min-height"};function a(i,d){const e=t("RouterView");return _(),n("div",r,[c(e)])}const p=o(s,[["render",a],["__scopeId","data-v-ee81f890"]]);export{p as default};
