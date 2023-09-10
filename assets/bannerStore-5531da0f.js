f3dbc09394af64784eb86a4c17e3b5b5bfb3f33e
kelvin <kelvin80121@hotmail.com>
2023-09-10 22:07:33 +0800

    deploy

---
+++ assets/bannerStore-5531da0f.js
@@ -0,0 +1,1 @@
+import{a0 as o}from"./index-fa4372e0.js";const b=o("bannerStore",{state:()=>({bgUrl:"",banner:{title:"",text:"",footer:""}}),actions:{getBannerInfo(t,e,n,r){this.bgUrl=t,this.banner.title=e,this.banner.text=n,this.banner.footer=r}}});export{b};
